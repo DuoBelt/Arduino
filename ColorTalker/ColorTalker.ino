70236c9607e0cdd9dcb5a03c854980cf9ebccda6
K.Seki <k-seki@klabo.co.jp>
2015-09-29 13:37:39 +0900

    ooo

--- ColorTalker/ColorTalker.ino
+++ ColorTalker/ColorTalker.ino
@@ -2,7 +2,7 @@
 // 釧路高専：カラー読み取り機
 //
 
-#define USE_S11059 false
+#define USE_S11059 true
 #define DEBUG_THIS true
 
 #include <Wire.h>
@@ -26,7 +26,9 @@ SoftwareSerial SWSerial(BLUETOOTH_RX,BLUETOOTH_TX);
 
 #define BUTTON 3
 
-void dummyRoutine(){
+static int pushCounter = 0;
+void checkTheButton(){
+  pushCounter++;
   return; 
 }
 
@@ -39,6 +41,7 @@ void readAndSendTheValue(){
   Wire.beginTransmission(0x2A);
   Wire.write(0x03);
   Wire.endTransmission();
+
   Wire.requestFrom(0x2A, 8);
   if (Wire.available()) {
     static char *name[] = {"R","G","B","I"};
@@ -84,9 +87,10 @@ void setup() {
 #endif
 
   pinMode(BUTTON,INPUT_PULLUP);
-  attachInterrupt(INT1,dummyRoutine,FALLING);
+  attachInterrupt(INT1,checkTheButton,FALLING);
 
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  set_sleep_mode(SLEEP_MODE_IDLE);
+//  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 //  set_sleep_mode(SLEEP_MODE_PWR_SAVE);
   sleep_enable();
 }
@@ -103,5 +107,8 @@ void loop() {
 //  Serial.println("Good morningt!");
 //  delay(1000);
 #endif
-  readAndSendTheValue();
+  if(pushCounter){
+    pushCounter = 0;
+    readAndSendTheValue();
+  }
 }
--- colorPicker/colorPicker.ino
+++ colorPicker/colorPicker.ino
@@ -1,10 +1,30 @@
+//
+// Color Picker
+//
+//  wiring ...
+//  D3 <--- tactile switch (PULL-UP)
+//  D5 <--- Bluetooth RX
+//  D6 ---> Bluetooth TX
+//  A4 ---> I2C - SDA
+//  A5 ---> I2C - SDC
+
 #include <Wire.h>
 #include <avr/sleep.h>
+#include <SoftwareSerial.h>
+
+#define BLUETOOTH_RX 5
+#define BLUETOOTH_TX 6
+
+#define WHITELED_PIN 4
+#define VIBRATOR_PIN 8
+
+SoftwareSerial SWSerial(BLUETOOTH_RX,BLUETOOTH_TX);
 
 void setup() {
-  Wire.begin();
   Serial.begin(115200);
-
+  SWSerial.begin(9600);
+  
+  Wire.begin();
   Wire.beginTransmission(0x2A);
   Wire.write(0x0);
   Wire.write(0x89);
@@ -16,30 +36,36 @@ void setup() {
 
   pinMode(3,INPUT_PULLUP);
   attachInterrupt(1,checkButton,FALLING);
+
+  pinMode(WHITELED_PIN,OUTPUT);
+  pinMode(VIBRATOR_PIN,OUTPUT);
+  
   set_sleep_mode(SLEEP_MODE_IDLE);
   sleep_enable();
 }
 
-static int count = 0;
+static int pushCounter = 0;
 void checkButton(){
-  count++;  
+  pushCounter++;  
 }
 
 void loop() {
   sleep_mode();
-  if(count){
-    count = 0;
-    colorread();
+  if(pushCounter){
+//    Serial.println(pushCounter);
+    pushCounter = 0;
+    sendColorData();
   }
 }
 
-void colorread() {
+void sendColorData() {
   Wire.beginTransmission(0x2A);
   Wire.write(0x03);
   Wire.endTransmission();
   Wire.requestFrom(0x2A, 8);
   if (Wire.available()) {
-    static char *name[] = {"R","G","B","I"};
+    static char *name[] = {"Red","Green","Blue","IR"};
+    int rgbi[sizeof(name)/sizeof(char *)];
     int HByte;
     int LByte;
     int value;
@@ -50,14 +76,17 @@ void colorread() {
       HByte = Wire.read();
       LByte = Wire.read();
       value = HByte << 8 | LByte;
+      rgbi[a] = value;
       Serial.print(name[a]);
       Serial.print(" = ");
       Serial.print(value);
       Serial.print(" ");
     }
     Serial.println("");
+    SWSerial.println("読み込み完了!");
   }
   Wire.endTransmission();
+  
 }
 
 
