61c0894c8ffb4a1fb3458efa43803cf08b10c957
K.Seki <k-seki@klabo.co.jp>
2015-11-03 07:36:20 +0900

    https://files.gitter.im/esp8266/Arduino/Abqa/libm.a.tbz
    でlibm.aをreplace

--- SSWroom02PlusBME280/SSWroom02PlusBME280.ino
+++ SSWroom02PlusBME280/SSWroom02PlusBME280.ino
@@ -1,42 +1,3 @@
-/*
-
-Copyright (c) 2015, Embedded Adventures
-All rights reserved.
-
-Contact us at source [at] embeddedadventures.com
-www.embeddedadventures.com
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-- Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-
-- Redistributions in binary form must reproduce the above copyright
-  notice, this list of conditions and the following disclaimer in the
-  documentation and/or other materials provided with the distribution.
-
-- Neither the name of Embedded Adventures nor the names of its contributors
-  may be used to endorse or promote products derived from this software
-  without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
-// BME280 MOD-1022 weather multi-sensor Arduino demo
-// Written originally by Embedded Adventures
-
 #include <ESP8266WiFi.h>
 #include <BME280_MOD-1022.h>
 #include <Wire.h>
@@ -51,9 +12,6 @@ void printFormattedFloat(float x, uint8_t precision) {
   Serial.print(buffer);
 
 }
-
-
-// print out the measurements
 
 void printCompensatedMeasurements(void) {
 
@@ -70,26 +28,6 @@ void printCompensatedMeasurements(void) {
   tempMostAccurate     = BME280.getTemperatureMostAccurate();
   humidityMostAccurate = BME280.getHumidityMostAccurate();
   pressureMostAccurate = BME280.getPressureMostAccurate();
-  //  Serial.println("                Good  Better    Best");
-  //  Serial.print("Temperature  ");
-  //  printFormattedFloat(temp, 2);
-  //  Serial.print("         ");
-  //  printFormattedFloat(tempMostAccurate, 2);
-  //  Serial.println();
-  //
-  //  Serial.print("Humidity     ");
-  //  printFormattedFloat(humidity, 2);
-  //  Serial.print("         ");
-  //  printFormattedFloat(humidityMostAccurate, 2);
-  //  Serial.println();
-  //
-  //  Serial.print("Pressure     ");
-  //  printFormattedFloat(pressure, 2);
-  //  Serial.print(" ");
-  //  printFormattedFloat(pressureMoreAccurate, 2);
-  //  Serial.print(" ");
-  //  printFormattedFloat(pressureMostAccurate, 2);
-  //  Serial.println();
 
   String nickname = "sekitakovich";
   String info = "t=" + String(tempMostAccurate) + "&h=" +  String(humidityMostAccurate) + "&p=" + String(pressureMostAccurate) + "&v=0" + "&nickname=" + nickname;
@@ -108,11 +46,6 @@ void printCompensatedMeasurements(void) {
   }
 }
 
-static unsigned int counter = 0;
-void onTouch() {
-  counter++;
-  digitalWrite(13, (counter) % 2);
-}
 void setup()
 {
   const char* ssid     = "ms101";
@@ -132,9 +65,9 @@ void setup()
 
   wifi_set_sleep_type(LIGHT_SLEEP_T);
 
-  pinMode(0, INPUT);
-  pinMode(13, OUTPUT);
-  attachInterrupt(0, onTouch, FALLING);
+//  pinMode(0, INPUT);
+//  pinMode(13, OUTPUT);
+//  attachInterrupt(0, sendThis, FALLING);
   //
   //  uint8_t chipID;
 
@@ -163,26 +96,6 @@ void setup()
   }
   Serial.println("Done!");
 
-  // read out the data - must do this before calling the getxxxxx routines
-  //  BME280.readMeasurements();
-  //  Serial.print("Temp=");
-  //  Serial.println(BME280.getTemperature());  // must get temp first
-  //  Serial.print("Humidity=");
-  //  Serial.println(BME280.getHumidity());
-  //  Serial.print("Pressure=");
-  //  Serial.println(BME280.getPressure());
-  //  Serial.print("PressureMoreAccurate=");
-  //  Serial.println(BME280.getPressureMoreAccurate());  // use int64 calculcations
-  //  Serial.print("TempMostAccurate=");
-  //  Serial.println(BME280.getTemperatureMostAccurate());  // use double calculations
-  //  Serial.print("HumidityMostAccurate=");
-  //  Serial.println(BME280.getHumidityMostAccurate()); // use double calculations
-  //  Serial.print("PressureMostAccurate=");
-  //  Serial.println(BME280.getPressureMostAccurate()); // use double calculations
-
-  // Example for "indoor navigation"
-  // We'll switch into normal mode for regular automatic samples
-
   BME280.writeStandbyTime(tsb_0p5ms);        // tsb = 0.5ms
   BME280.writeFilterCoefficient(fc_16);      // IIR Filter coefficient 16
   BME280.writeOversamplingPressure(os16x);    // pressure x16
@@ -191,12 +104,10 @@ void setup()
 
   BME280.writeMode(smNormal);
 
-  //
 }
 
-void loop()
-{
-  //  Serial.println("loop!");
+void sendThis() {
+  digitalWrite(13,HIGH);
   while (BME280.isMeasuring()) {
     delay(1);
   }
@@ -204,7 +115,12 @@ void loop()
   BME280.readMeasurements();
   printCompensatedMeasurements();
 
-  delay(1000 * 60);	// do this every 5 seconds
-  //  Serial.println();
+  digitalWrite(13,LOW);
+}
+
+void loop()
+{
+  sendThis();
+  delay(1000 * 60 * 3);	// do this every n min
 }
 
---
+++ WROOM02_bme280_withAdafruitLibs/WROOM02_bme280_withAdafruitLibs.ino
@@ -0,0 +1,63 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <ESP8266WiFi.h>
+extern "C" {
+#include "user_interface.h"
+}
+
+Adafruit_BME280 bme; // I2C
+
+#define DELAYMS (1000 * 60 * 3)
+#define LEDPIN (13)
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Start collection");
+
+  pinMode(LEDPIN,OUTPUT);
+  
+  char *ssid = "ms101";
+  char *password = "sekitakovich";
+
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000*1);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  bme.begin(0x76);
+}
+
+void loop() {
+  float t = bme.readTemperature();
+  float p = bme.readPressure() / 100.0F;
+  float h =  bme.readHumidity();
+  float v = 0.0;
+
+  digitalWrite(LEDPIN,HIGH);
+  
+  String nickname = "sekitakovich";
+  String info = "t=" + String(t) + "&h=" +  String(h) + "&p=" + String(p) + "&v=" + String(v) + "&nickname=" + nickname;
+  Serial.println(info);
+
+  char *host = "www.klabo.co.jp";
+  WiFiClient client;
+  int httpPort = 80;
+  if (client.connect(host, httpPort)) {
+    String url = String("/tph.php?")  + info;
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+  }
+
+  digitalWrite(LEDPIN,LOW);
+  
+  delay(DELAYMS);
+}
