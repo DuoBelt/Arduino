d7934ce535e65c315e5135b672315594f3706173
K.Seki <k-seki@klabo.co.jp>
2016-07-12 07:26:59 +0900

    plus 1

--- WROOM-02/b24/loop.ino
+++ WROOM-02/b24/loop.ino
@@ -5,32 +5,33 @@ void loop() {
   float current_mA = ina219.getCurrent_mA();
   float loadvoltage = busvoltage + (shuntvoltage / 1000);
 
-  float t = bme.readTemperature();
-  float p = bme.readPressure() / 100.0F;
-  float h = bme.readHumidity();
-
-  static char host[] = "www.klabo.co.jp";
-  char thisURL[0x100];
-
   bool liteSleep =  ((loadvoltage > lastSendV) || (current_mA > lastSendA));
 
-  String getOption = "t=" + String(t) + "&h=" +  String(h) + "&p=" + String(p) + "&mac=" + thisMAC + "&up=" + upCount++ + "&spv=" + String(loadvoltage, 3) + "&spa=" + String(current_mA, 3);
-  String getString = String("/tph.php?")  + getOption;
+  if (liteSleep) {
+    float t = bme.readTemperature();
+    float p = bme.readPressure() / 100.0F;
+    float h = bme.readHumidity();
 
-  getString.toCharArray(thisURL, sizeof(thisURL));
+    static char host[] = "www.klabo.co.jp";
+    char thisURL[0x100];
 
-  uploadThis(host, thisURL, 80);
+    String getOption = "t=" + String(t) + "&h=" +  String(h) + "&p=" + String(p) + "&mac=" + thisMAC + "&up=" + upCount++ + "&spv=" + String(loadvoltage, 3) + "&spa=" + String(current_mA, 3);
+    String getString = String("/tph.php?")  + getOption;
 
-  if(liteSleep){
-    delay(5*1000);
+    getString.toCharArray(thisURL, sizeof(thisURL));
+
+    uploadThis(host, thisURL, 80);
+
+    lastSendV = loadvoltage;
+    lastSendA = current_mA;
+
+    delay(5 * 1000);
   }
-  else{
+  else {
     Serial.println("Good night!");
-     ESP.deepSleep(60 * 1000 * 1000 , WAKE_RF_DEFAULT);
-     delay(1000);
+    ESP.deepSleep(60 * 1000 * 1000 , WAKE_RF_DEFAULT);
+    delay(1000);
   }
 
-  lastSendV = loadvoltage;
-  lastSendA = current_mA;
 }
 
