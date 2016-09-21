566f04d105288ae8070a2ef8d99eabf4f0358308
Katsunari Seki <k-seki@klabo.co.jp>
2016-09-21 17:48:14 +0900

    0921

---
+++ ProTrinket3V/activityChecker_SLEEP_PCI/activityChecker_SLEEP_PCI.ino
@@ -0,0 +1,124 @@
+#define SS_RX (5)
+#define SS_TX (6)
+
+#include <SparkFun_ADXL345.h>
+#include <avr/sleep.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include <SoftwareSerial.h>
+#define USE_AVOLUME false
+
+ADXL345 adxl = ADXL345();
+
+SoftwareSerial ss(SS_RX, SS_TX);
+
+int interruptPin = 8; // notice! Pin-Change-Interrupt
+volatile int lastValue = LOW;
+
+volatile bool busyINT = false;
+bool prevActivity = false;
+unsigned int counter = 0;
+
+#if USE_VOLUME
+byte volumeA = 0;
+byte volumeI = 0;
+#else
+#define ACT_TH (60)
+byte volumeA = ACT_TH;
+byte volumeI = ACT_TH;
+#endif
+
+#define PIN_LED (13)
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("check the ACTIVITY start!");
+
+  //  pinMode(SS_RX,INPUT);
+  //  pinMode(SS_TX,OUTPUT);
+  ss.begin(115200);
+
+  adxl.powerOn();
+  adxl.setRangeSetting(16);
+
+  adxl.setActivityXYZ(1, 1, 1);       // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setActivityThreshold(volumeA);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
+
+  adxl.setInactivityXYZ(1, 1, 1);     // Set to detect inactivity in all the axes "adxl.setInactivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setInactivityThreshold(volumeI);    // 62.5mg per increment   // Set inactivity // Inactivity thresholds (0-255)
+  adxl.setTimeInactivity(1);         // How many seconds of no activity is inactive?
+
+  adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);"
+  // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
+  // This library may have a problem using INT2 pin. Default to INT1 pin.
+
+  // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
+  adxl.InactivityINT(1);
+  adxl.ActivityINT(1);
+  adxl.FreeFallINT(0);
+  adxl.doubleTapINT(0);
+  adxl.singleTapINT(0);
+
+  byte interrupts = adxl.getInterruptSource(); // Why ????????????????????????????????
+//  attachInterrupt(digitalPinToInterrupt(interruptPin), thisISR, RISING);   // Attach Interrupt
+
+  PCMSK2 |= (1 << PCINT0); // on D8
+  PCICR  |= (1 << PCIE0); // enable interrupt from group PCIE2
+
+  pinMode(PIN_LED, OUTPUT);
+
+  //  set_sleep_mode(SLEEP_MODE_IDLE);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+}
+
+//void thisISR() {
+//  busyINT = true;
+//}
+
+void loop() {
+#if USE_VOLUME
+  volumeA = analogRead(A0) / 4;
+  volumeI = analogRead(A1) / 4;
+  adxl.setActivityThreshold(volumeA);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
+  adxl.setInactivityThreshold(volumeI);    // 62.5mg per increment   // Set inactivity // Inactivity thresholds (0-255)
+#endif
+  if (lastValue == HIGH) {
+    lastValue = LOW;
+    digitalWrite(PIN_LED, HIGH);
+    //
+    byte interrupts = adxl.getInterruptSource();
+    bool thisActivity = false;
+
+    if (adxl.triggered(interrupts, ADXL345_ACTIVITY)) {
+      thisActivity = true;
+    }
+    /*
+        else if (adxl.triggered(interrupts, ADXL345_INACTIVITY)) {
+          thisActivity = false;
+        }
+    */
+    if (thisActivity != prevActivity) {
+      prevActivity = thisActivity;
+      if (thisActivity) {
+        counter++;
+        Serial.println("Activity " + String(counter));
+        ss.println(counter);
+        delay(10);
+      }
+    }
+    //
+    digitalWrite(PIN_LED, LOW);
+  }
+  else {
+    sleep_mode();
+  }
+}
+
+ISR(PCINT0_vect) {
+  lastValue = digitalRead(interruptPin);
+}
+
+
+
