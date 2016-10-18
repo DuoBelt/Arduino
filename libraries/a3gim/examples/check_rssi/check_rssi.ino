8bf49ec55873dbe0b3efc0fcdbecd5b0593144fa
K.Seki <k-seki@klabo.co.jp>
2016-10-18 09:12:35 +0900

    1016

---
+++ libraries/a3gim/examples/check_rssi/check_rssi.ino
@@ -0,0 +1,37 @@
+// 3GIM(V2) sample skech -- getRSSI
+
+#include <SoftwareSerial.h>
+#include "a3gim.h"
+
+#define baudrate 	9600UL
+const int powerPin = 7;     // 3gim power pin(If not using power control, 0 is set.)
+
+void setup()
+{
+  Serial.begin(baudrate);
+  delay(3000);  // Wait for Start Serial Monitor
+  Serial.println("Ready.");
+
+  Serial.print("Initializing.. ");
+  if (a3gs.start(powerPin) == 0 && a3gs.begin(0, baudrate) == 0) {
+    Serial.println("Succeeded.");
+    int rssi;
+    if (a3gs.getRSSI(rssi) == 0) {
+      Serial.print("RSSI = ");
+      Serial.print(rssi);
+      Serial.println(" dBm");
+    }
+  }
+  else
+    Serial.println("Failed.");
+
+  Serial.println("Shutdown..");
+  a3gs.end();
+  a3gs.shutdown();
+}
+
+void loop()
+{
+}
+
+// END
